182c7808cbd6dbabea0f8c5e17ce65b7fd1175da
Elkin Contreras <escontreras-2023a@corhuila.edu.co>
2025-03-31 21:12:00 -0500

    docs: project code correction

--- 08-week/src/App.tsx
+++ 08-week/src/App.tsx
@@ -1,31 +1,40 @@
-import { Redirect, Route } from "react-router-dom";
-import { IonApp, IonRouterOutlet, setupIonicReact } from "@ionic/react";
-import { IonReactRouter } from "@ionic/react-router";
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import FormularioPersona from "./pages/FormularioPersona"; 
+import { Redirect, Route } from 'react-router-dom';
+import { IonApp, IonRouterOutlet, setupIonicReact } from '@ionic/react';
+import { IonReactRouter } from '@ionic/react-router';
+import Home from './pages/Home';
+import Cliente from './pages/Cliente';
+import Empleado from './pages/Empleado';
+
 
 /* Core CSS required for Ionic components to work properly */
-import "@ionic/react/css/core.css";
+import '@ionic/react/css/core.css';
 
 /* Basic CSS for apps built with Ionic */
-import "@ionic/react/css/normalize.css";
-import "@ionic/react/css/structure.css";
-import "@ionic/react/css/typography.css";
+import '@ionic/react/css/normalize.css';
+import '@ionic/react/css/structure.css';
+import '@ionic/react/css/typography.css';
 
 /* Optional CSS utils that can be commented out */
-import "@ionic/react/css/padding.css";
-import "@ionic/react/css/float-elements.css";
-import "@ionic/react/css/text-alignment.css";
-import "@ionic/react/css/text-transformation.css";
-import "@ionic/react/css/flex-utils.css";
-import "@ionic/react/css/display.css";
+import '@ionic/react/css/padding.css';
+import '@ionic/react/css/float-elements.css';
+import '@ionic/react/css/text-alignment.css';
+import '@ionic/react/css/text-transformation.css';
+import '@ionic/react/css/flex-utils.css';
+import '@ionic/react/css/display.css';
 
-/* Ionic Dark Mode */
-import "@ionic/react/css/palettes/dark.system.css";
+/**
+ * Ionic Dark Mode
+ * -----------------------------------------------------
+ * For more info, please see:
+ * https://ionicframework.com/docs/theming/dark-mode
+ */
+
+/* import '@ionic/react/css/palettes/dark.always.css'; */
+/* import '@ionic/react/css/palettes/dark.class.css'; */
+import '@ionic/react/css/palettes/dark.system.css';
 
 /* Theme variables */
-import "./theme/variables.css";
+import './theme/variables.css';
 
 setupIonicReact();
 
@@ -33,17 +42,18 @@ const App: React.FC = () => (
   <IonApp>
     <IonReactRouter>
       <IonRouterOutlet>
-        <Route exact path="/login">
-          <Login />
-        </Route>
         <Route exact path="/home">
           <Home />
         </Route>
-        <Route exact path="/formulario-persona"> {/* Nueva ruta */}
-          <FormularioPersona />
+        <Route exact path="/cliente">
+          <Cliente />
         </Route>
+        <Route exact path="/empleado">
+          <Empleado />
+        </Route>
+        
         <Route exact path="/">
-          <Redirect to="/login" /> {/* Redirige al Login por defecto */}
+          <Redirect to="/home" />
         </Route>
       </IonRouterOutlet>
     </IonReactRouter>
